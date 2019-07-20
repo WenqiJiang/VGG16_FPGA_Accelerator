8012b57d0f1b70dc4829e8ee6c512c7769e1a498
24Kcool <kx2141@columbia.edu>
2019-07-19 20:44:39 -0400

    Finish Debug Software
    After several weeks hard time, software part is finished now. It can load data to FPGA part and teach FPGA how to use the data now. At present, we only implement convolution layer and retrieve convolution result into software for FC layer.

---
+++ VGG_H&S_implementation/Software/load_fixed.h
@@ -0,0 +1,3 @@
+unsigned short str2fixed(char* input, int data_len);
+void load_data_local(char const* fname, char* bin_char);
+int load_fixed(char* fname, int data_num, unsigned short *fixed_point);
